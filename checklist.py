d54d5a9f2aa65e01baa02572f182be6610eb15dd
Jimmy Vu <JMV3211@gmail.com>
2024-10-07 21:36:38 -0700

    create helper functions

--- checklist.py
+++ checklist.py
@@ -1,41 +1,79 @@
 checklist = list()
 
 # CREATE
-
-
 def create(item):
     checklist.append(item)
 
 # READ
-
-
 def read(index):
-  return checklist[index]
+    return checklist[index]
 
 # UPDATE
-
-
 def update(index, item):
     checklist[index] = item
 
 # DESTROY
-
-
 def destroy(index):
-   checklist.pop(index)
+    checklist.pop(index)
 
+def list_all_items():
+    index = 0
+    for list_item in checklist:
+        print("{}: {}".format(index, list_item))
+        index += 1
+
+def mark_completed(index):
+    update(index, f"√{checklist[index]}")
+
+def user_input(prompt):
+    # the input function will display a message in the terminal
+    # and wait for user input.
+    user_input = input(prompt)
+    return user_input
+
+
+def select(function_code):
+    # Create item
+    if function_code == "C":
+        input_item = user_input("Input item:")
+        create(input_item)
+
+    # Read item
+    elif function_code == "R":
+        item_index = int(user_input("Index Number?"))
+
+        # Remember that item_index must actually exist or our program will crash.
+        read(item_index)
+
+    # Print all items
+    elif function_code == "P":
+        list_all_items()
+
+    # Catch all
+    else:
+        print("Unknown Option")
 
 def test():
-   create("purple sox")
-   create("red cloak")
+    create("purple sox")
+    create("red cloak")
 
-   print(read(0))
-   print(read(1))
+    print(read(0))
+    print(read(1))
 
-   update(0, "purple socks")
-   destroy(1)
+    update(0, "purple socks")
+    destroy(1)
 
-   print(read(0))
-  #  print(read(1))
+    print(read(0))
+
+    list_all_items()
+
+    select("C")
+    # View the results
+    list_all_items()
+    # Call function with new value
+    select("R")
+    # View results
+    list_all_items()
+
 
 test()
